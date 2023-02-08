4fdd763f933fa6f8b19ec609407bb6981da25968
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-07 21:47:32 -0500

    clue #1

--- answers.sql
+++ answers.sql
@@ -25,7 +25,7 @@
 
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
 
-
+SELECT MIN(population), name FROM country ORDER BY population DESC;    
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
