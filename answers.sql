c2095a05e03e68fe3fad51f9b7030e0ceffdd117
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-08 21:37:32 -0500

    1st clue solved

--- answers.sql
+++ answers.sql
@@ -25,8 +25,7 @@
 
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
 
-SELECT name, population FROM country WHERE region = 'Southern Europe';   
-
+-- SELECT * FROM country WHERE region = 'Southern Europe';
 -- Holy See (Vatican City State)
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
