e936d8f8c6a78e17e78dcdcde465644faa915122
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-08 21:55:55 -0500

    4th clue solved

--- answers.sql
+++ answers.sql
@@ -40,8 +40,8 @@
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
-
-
+-- SELECT city.name FROM city WHERE city.countrycode = 'SMR';
+-- Serravalle
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
