83eb3f5b17edc88c656d4572958324906caba691
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-08 21:45:15 -0500

    2nd clue solved

--- answers.sql
+++ answers.sql
@@ -30,8 +30,8 @@
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
-
-
+-- SELECT countrylanguage.countrycode, countrylanguage.language FROM countrylanguage WHERE countrycode = 'VAT';
+-- Italian
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
