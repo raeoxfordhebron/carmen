1f6ee092e9c3d161dd1aac66174ec23e53675154
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-08 21:52:09 -0500

    3rd clue solved

--- answers.sql
+++ answers.sql
@@ -35,8 +35,8 @@
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
-
-
+ -- SELECT cl.countrycode, cl.language, cl.percentage FROM countrylanguage cl WHERE cl.language = 'Italian' and cl.isofficial = true;
+ -- San Marino
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
