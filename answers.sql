569e696c87fd1eb75ca3ec68586c1b0555974099
raeoxfordhebron <raeoxfordhebron@gmail.com>
2023-02-13 14:12:21 -0500

    solved 6th clue

--- answers.sql
+++ answers.sql
@@ -53,7 +53,8 @@
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
 
-
+-- SELECT city.name, city.countrycode FROM "city" INNER JOIN "country" on country.capital = city.id
+-- Brasilia
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
