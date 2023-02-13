8924b5161344f3f4a9a96d8bbfec21242c29c403
raeoxfordhebron <raeoxfordhebron@gmail.com>
2023-02-13 14:19:31 -0500

    solved 7th clue

--- answers.sql
+++ answers.sql
@@ -71,7 +71,8 @@
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
-
+-- SELECT city.name, city.population FROM "city" WHERE city.population = 91084;
+-- Santa Monica
 
 
 -- She's in ____________________________!
